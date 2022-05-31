ea1dcbbb74e87c8ca45c5c349102d16a2a25536c
abraham-kwizera <kwiz.abraham@gmail.com>
2022-05-31 20:50:28 +0200

    Render data with prev and next button render pokeman names list created pagination file which allow us to move from one page to another create previous and next button
    wip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
+import Pagination from "./Pagination";
 import axios from "axios";
 
 function App() {
-  useState(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-      setPokemon(res.data.results.map(output => output.name))
-    });
-  });
-
+  // VARIABLE DECLARATIONS
   const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState([]);
+  const [prevPageUrl, setPrevPageUrl] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  // FUNCTIONS
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: axios.CancelToken((cancelItem) => (cancel = cancelItem)),
+      })
+      .then((res) => {
+        setLoading(false); 
+        setPrevPageUrl(res.data.previous);
+        setNextPageUrl(res.data.next);
+        setPokemon(res.data.results.map((output) => output.name));
+
+        return () => cancel();
+      });
+  }, [currentPageUrl]);
+
+  
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  if (loading) return "Loading...";
+
   return (
-    // render lists of Pokemon
-    <PokemonList pokemon={pokemon} />
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination 
+      goToPrevPage={prevPageUrl ? goToPrevPage : null } 
+      goToNextPage={nextPageUrl ? goToNextPage : null} 
+      />
+    </>
   );
 }
 

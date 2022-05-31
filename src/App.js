137a4b0629f5281a4ff53dd3ddaf13a1b0739597
abraham-kwizera <kwiz.abraham@gmail.com>
2022-05-31 10:42:17 +0200

    Request data from pokeAPI installed axios package requested data from pokeapi
    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, {useState} from 'react';
-import PokemonList from './PokemonList';
+import React, { useState } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "ivysaur"])
+  useState(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+      setPokemon(res.data.results.map(output => output.name))
+    });
+  });
+
+  const [pokemon, setPokemon] = useState([]);
   return (
-    
     // render lists of Pokemon
-    <PokemonList pokemon={pokemon}/>
+    <PokemonList pokemon={pokemon} />
   );
 }
 
